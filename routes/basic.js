e182e117ab3ab963f28607bb4a90cd0219296c23
Viraj24Gupta <viraj24Gupta@gmailcom>
2021-05-09 21:24:36 +0530

    url formed

--- routes/basic.js
+++ routes/basic.js
@@ -13,15 +13,12 @@ router.post('/', async(req,res)=>{
     try{
         dist = req.body.district;
         dat = (req.body.date).toString().split("-").reverse().join("-");
-        console.log(dist);
-        console.log(dat);
     }
     catch (err) {
         await res.json({message: err});
     }
+    let url=  "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=" + String(dist) + "&date=" + String(dat);
+    console.log(url);
 });
-
-let url=  "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=" + dist + "&date=" + dat;
-
 
 module.exports = router;
