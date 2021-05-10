504171549148cbfb7db36375f6cfb0220841d929
Viraj24Gupta <viraj24Gupta@gmailcom>
2021-05-10 12:16:39 +0530

    corrected working

--- routes/basic.js
+++ routes/basic.js
@@ -1,5 +1,7 @@
 const express= require('express');
 const router = express.Router();
+const got = require('got');
+const ejs = require('ejs');
 
 let dist;
 let dat;
@@ -13,12 +15,28 @@ router.post('/', async(req,res)=>{
     try{
         dist = req.body.district;
         dat = (req.body.date).toString().split("-").reverse().join("-");
+
+        console.log(dist,dat);
     }
     catch (err) {
         await res.json({message: err});
     }
     let url=  "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=" + String(dist) + "&date=" + String(dat);
     console.log(url);
+    (async ()=>{
+        try {
+            const response = await got(url);
+            // await res.json(response.body);
+            let data = response.body;
+            router.get('/result', (req, res)=> {
+                res.render('result',{data : data});
+            });
+
+        }
+        catch (err) {
+            console.log(err);
+        }
+    })();
 });
 
 module.exports = router;
